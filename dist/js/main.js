8240d2d7c5ae6b2decd93a6e058d8b7d11e5f16c
Travis Reynolds <travisreynolds17@gmail.com>
2018-10-14 18:26:21 -0400

    Button hint JS

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,9 +12,17 @@ const btnScramble = document.querySelector("#btn-scramble");
 const btnAlpha = document.querySelector("#btn-alpha");
 const btnAlphaRev = document.querySelector("#btn-reverse-alpha");
 const btnSub = document.querySelector("#btn-substitution");
+const btnReverse = document.querySelector("#btn-reverse");
+
+//descriptions for selected task
+const descScramble = "This is for scramble.";
+const descAlpha = "This is for alphabetize.";
+const descAlphaRev = "This is for reverse alphabetize.";
+const descSub = "This is for substitution.";
+const descReverse = "This is for reverse.";
 
 // collective button array
-let buttons = [btnScramble, btnAlpha, btnAlphaRev, btnSub];
+let buttons = [btnScramble, btnAlpha, btnAlphaRev, btnSub, btnReverse];
 
 let currentState = null;
 
@@ -32,4 +40,36 @@ function setState(state) {
   buttons.forEach(item => item.classList.remove("show"));
   state.classList.add("show");
   currentState = state.id;
+  setDescription(currentState);
+}
+
+function setDescription(state) {
+  switch (state) {
+    case "btn-scramble":
+      render(btnHint, descScramble);
+      break;
+
+    case "btn-alpha":
+      render(btnHint, descAlpha);
+      break;
+
+    case "btn-reverse-alpha":
+      render(btnHint, descAlphaRev);
+      break;
+
+    case "btn-substitution":
+      render(btnHint, descSub);
+      break;
+
+    case "btn-reverse":
+      render(btnHint, descReverse);
+      break;
+
+    default:
+      break;
+  }
+}
+// inject html at a specified node
+function render(node, template) {
+  node.innerHTML = template;
 }
